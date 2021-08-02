eb20bda711c504f19fd44774a4ef2919b735ac59
Mohamed Elsayed <eng.mohabbas@gmail.com>
2021-08-02 13:15:30 +0200

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Two functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
+
+## Creates a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+              ## initialize the inverse property
+              m <- NULL
+              
+              ## setting the matrix
+              set <- function(matrix) {
+                x <<- matrix
+                m <<- NULL
+              }
+              
+              ## getting the matrix
+              get <- function(){
+                ## return the matrix
+                x
+              }
+              
+              ## set the inverse of matrix
+              
+              setinverse <- function(inverse){
+                m <<- inverse
+              }
+              
+              ## get the inverse of matrix
+              
+              getinverse <- function(){
+                
+                ## return inverse
+                m
+              }
+              
+              ## return the list of methods
+              
+              list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+              
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        
+        if(!is.null(i)){
+          message("getting cached data")
+          return(i)
+        }
+        
+        ## Get the matrix from our object
+        data <- x$get()
+        
+        ## Calculate the inverse using matrix multiplication
+        i <- solve(data) %*% data
+        
+        ## Set the inverse to the object
+        x$setInverse(i)
+        
+        i
+        
 }
+## recommitting the project
